e76773d6033ca6e0fef09e797fca1e2487ac26bb
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2023-12-21 18:16:00 +0530

    added dish details

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,12 +1,18 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -14,7 +20,25 @@ import androidx.compose.ui.unit.dp
 fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        //TODO: Insert code here
+        TopAppBar()
+        Image(
+            painter = painterResource(id = dish.imageResource),
+            contentDescription = "Dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth)
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = dish.name, style = MaterialTheme.typography.h4)
+            Text(text = dish.description, style = MaterialTheme.typography.body1)
+            Counter()
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.add_for) + " $${dish.price}", textAlign = TextAlign.Center)
+            }
+        }
+
     }
 }
 
@@ -29,17 +53,17 @@ fun Counter() {
         }
         TextButton(
             onClick = {
-                counter--
+                if (counter > 0) counter--
             }
         ) {
             Text(
                 text = "-",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.h4
             )
         }
         Text(
             text = counter.toString(),
-            style = MaterialTheme.typography.h2,
+            style = MaterialTheme.typography.h4,
             modifier = Modifier.padding(16.dp)
         )
         TextButton(
@@ -49,7 +73,7 @@ fun Counter() {
         ) {
             Text(
                 text = "+",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.h4
             )
         }
     }
